86e45900af29f7382854468f09f8e37cb091ca9e
Jinxx6_6 <135565126+jinix6@users.noreply.github.com>
2023-12-16 09:09:03 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -12,6 +12,7 @@ import zipfile
 from io import BytesIO
 import random
 import string
+from sticker_sticker_pack_cache import add_data_to_json, get_user_data, delete_data_from_json
 
 TOKEN = os.getenv('TELEGRAM_BOT_API_ID')
 bot = telebot.TeleBot(TOKEN)
@@ -21,7 +22,7 @@ bot_username = bot_info.username
 user_states = {}
 user_data = {}
 HOME, STICKER_PACK_TITLE, APNG_TO_WEBM, ADD_STICKER, ADD_LINK_STICKER, DELPACK, STICKER_DOWNLOAD, DELSTICKER = range(8)
-
+send_st_pack_link_text = telebot.types.ForceReply(input_field_placeholder="🔗 Pᴀsᴛᴇ Yᴏᴜʀ Sᴛɪᴄᴋᴇʀ Pᴀᴄᴋ Lɪɴᴋ Hᴇʀᴇ:")
 sticker_pack_cre_mess = "🔥 Sᴛɪᴄᴋᴇʀ Pᴀᴄᴋ Cʀᴇᴀᴛᴇᴅ."
 jinxx_mess_start = """
 𝗔𝗣𝗡𝗚 𝗧𝗢 𝗪𝗘𝗕𝗠 𝗕𝗢𝗧 𝗕𝗬 👾𝗝𝗜𝗡𝗫𝗫
@@ -45,8 +46,12 @@ jinxx_mess_start = """
 
 
 
-
-
+def check_link(message):
+    if "https://t.me/addstickers/" in message:
+        hs72bsiqjb = True
+    else:
+        hs72bsiqjb = False
+    return hs72bsiqjb
 
 def generate_random_string(length=10):
     characters = string.ascii_letters + string.digits
@@ -111,66 +116,70 @@ def apng_to_webm(input_apng, output_webm, sticker_main_size):
 
 @bot.message_handler(content_types=['document'], func=lambda message: user_states.get(message.chat.id) == APNG_TO_WEBM)
 def handle_document(message):
-    try:
-        bot.send_chat_action(message.chat.id, 'typing')
-        bot.reply_to(message, f"🍥 Pʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ APNG ғɪʟᴇ...")
-        file_info = bot.get_file(message.document.file_id)
-        bot.send_chat_action(message.chat.id, 'upload_document')
-        downloaded_file = bot.download_file(file_info.file_path)
-        f72hs = message.from_user.id
-        with open(f"{f72hs}.apng", "wb") as file:
-            file.write(downloaded_file)
-    except Exception as e:
-        bot.send_chat_action(message.chat.id, 'typing')
-        bot.send_message(message.chat.id, e)
-    try:
-        sticker_main_size = get_apng_size(f"{f72hs}.apng")
-        webm_size, new_width, new_height = apng_to_webm(f"{f72hs}.apng", f"{f72hs}.webm", sticker_main_size)
-        # Send the WebM file
-        with open(f"{f72hs}.webm", 'rb') as sticker_file:
+    if message.document.mime_type == 'video/webm':
+        try:
+            bot.send_chat_action(message.chat.id, 'typing')
+            bot.reply_to(message, f"🍥 Pʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ APNG ғɪʟᴇ...")
+            file_info = bot.get_file(message.document.file_id)
             bot.send_chat_action(message.chat.id, 'upload_document')
-            sent_message = bot.send_document(message.chat.id, sticker_file)
-            file_id = sent_message.document.file_id
-            file_path = bot.get_file(file_id).file_path
-            base_url = 'https://api.telegram.org/file/bot' + TOKEN
-            full_raw_link = base_url + '/' + file_path
-            preju83 = f"https://jinix6.github.io/Webm_preview?video={full_raw_link}"
-            markup = types.InlineKeyboardMarkup()
-            webApp = types.WebAppInfo(preju83)
-            button = types.InlineKeyboardButton(text="👁️Pʀᴇᴠɪᴇᴡ", web_app=webApp)
-            markup.add(button)
+            downloaded_file = bot.download_file(file_info.file_path)
+            f72hs = message.from_user.id
+            with open(f"{f72hs}.apng", "wb") as file:
+                file.write(downloaded_file)
+        except Exception as e:
             bot.send_chat_action(message.chat.id, 'typing')
-            bot.send_message(message.chat.id, "🔘 Cʟɪᴄᴋ Tʜᴇ Bᴜᴛᴛᴏɴ Tᴏ Vɪsɪᴛ Tʜᴇ Pʀᴇᴠɪᴇᴡ Pᴀɢᴇ:", reply_markup=markup)
+            bot.send_message(message.chat.id, e)
+        try:
+            sticker_main_size = get_apng_size(f"{f72hs}.apng")
+            webm_size, new_width, new_height = apng_to_webm(f"{f72hs}.apng", f"{f72hs}.webm", sticker_main_size)
+        # Send the WebM file
+            with open(f"{f72hs}.webm", 'rb') as sticker_file:
+                bot.send_chat_action(message.chat.id, 'upload_document')
+                sent_message = bot.send_document(message.chat.id, sticker_file)
+                file_id = sent_message.document.file_id
+                file_path = bot.get_file(file_id).file_path
+                base_url = 'https://api.telegram.org/file/bot' + TOKEN
+                full_raw_link = base_url + '/' + file_path
+                preju83 = f"https://jinix6.github.io/Webm_preview?video={full_raw_link}"
+                markup = types.InlineKeyboardMarkup()
+                webApp = types.WebAppInfo(preju83)
+                button = types.InlineKeyboardButton(text="👁️Pʀᴇᴠɪᴇᴡ", web_app=webApp)
+                markup.add(button)
+                bot.send_chat_action(message.chat.id, 'typing')
+                bot.send_message(message.chat.id, "🔘 Cʟɪᴄᴋ Tʜᴇ Bᴜᴛᴛᴏɴ Tᴏ Vɪsɪᴛ Tʜᴇ Pʀᴇᴠɪᴇᴡ Pᴀɢᴇ:", reply_markup=markup)
             # Send the size information
-            size_info = f"📏 WᴇʙM Sɪᴢᴇ: {webm_size} bytes\n📏 Rᴇsɪᴢᴇᴅ Dɪᴍᴇɴsɪᴏɴs: {new_width}x{new_height}"
+                size_info = f"📏 WᴇʙM Sɪᴢᴇ: {webm_size} bytes\n📏 Rᴇsɪᴢᴇᴅ Dɪᴍᴇɴsɪᴏɴs: {new_width}x{new_height}"
+                bot.send_chat_action(message.chat.id, 'typing')
+                bot.send_message(message.chat.id, size_info, reply_to_message_id=sent_message.message_id)
+
+        except Exception as e:
             bot.send_chat_action(message.chat.id, 'typing')
-            bot.send_message(message.chat.id, size_info, reply_to_message_id=sent_message.message_id)
+            bot.send_message(message.chat.id, e)
 
-    except Exception as e:
+        try:
+            os.remove(f"{f72hs}.apng")
+            os.remove(f"{f72hs}.webm")
+        except Exception as e:
+            bot.send_chat_action(message.chat.id, 'typing')
+            bot.send_message(message.chat.id, e)
+
+    else:
         bot.send_chat_action(message.chat.id, 'typing')
-        bot.send_message(message.chat.id, e)
-
-    try:
-        os.remove(f"{f72hs}.apng")
-        os.remove(f"{f72hs}.webm")
-    except Exception as e:
-        bot.send_chat_action(message.chat.id, 'typing')
-        bot.send_message(message.chat.id, e)
+        bot.send_message(message.chat.id, "❌ Oɴʟʏ Sᴜᴘᴘᴏʀᴛ Wᴇʙᴍ Fɪʟᴇs. Pʟᴇᴀsᴇ Uᴘʟᴏᴀᴅ A Vᴀʟɪᴅ Wᴇʙᴍ Fɪʟᴇ.", parse_mode="Markdown")
 
 
-
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['cancel', 'start'])
 def start_fun(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message( message.chat.id, jinxx_mess_start, parse_mode="Markdown")
+    bot.send_chat_action(message.chat.id, 'typing') 
+    user_id = str(message.from_user.id)
+    result_data = get_user_data(user_id)
     user_states[message.chat.id] = HOME
-    
-
-@bot.message_handler(commands=['cancel'])
-def start_fun(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message( message.chat.id, jinxx_mess_start, parse_mode="Markdown")
-    user_states[message.chat.id] = HOME
+    if result_data:
+        formatted_links = [f'[▒ 🖇 𝗦𝘁𝗶𝗰𝗸𝗲𝗿 𝗣𝗮𝗰𝗸 𝗟𝗶𝗻𝗸 ▒]({link})' for link in result_data]
+        result = "\n".join(formatted_links)
+        bot.send_message(message.chat.id, f"{jinxx_mess_start}\n⚡⃨ 𝗖⃨𝗥⃨𝗘⃨𝗔⃨𝗧⃨𝗘⃨𝗗⃨ 𝗦⃨𝗧⃨𝗜⃨𝗖⃨𝗞⃨𝗘⃨𝗥⃨ 𝗣⃨𝗔⃨𝗖⃨𝗞⃨ 𝗟⃨𝗜⃨𝗦⃨𝗧⃨\n{result}", parse_mode="Markdown")
+    else:
+        bot.send_message(message.chat.id, f"{jinxx_mess_start}", parse_mode="Markdown")
 
     
 @bot.message_handler(commands=['newpack'])
@@ -253,29 +262,47 @@ def handle_sticker(message):
         bot.send_message(message.chat.id, e)
 
 
-        
+
+
+
 @bot.message_handler(commands=['delpack'])
 def create_sticker_pack(message):
     user_states[message.chat.id] = DELPACK
     bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message(message.chat.id, "🔗 Sᴇɴᴅ Sᴛɪᴄᴋᴇʀ Pᴀᴄᴋ Lɪɴᴋ")
-
+    bot.send_message(message.chat.id, "🔗 Sᴇɴᴅ Sᴛɪᴄᴋᴇʀ Pᴀᴄᴋ Lɪɴᴋ", reply_markup=send_st_pack_link_text)
+    
 @bot.message_handler(func=lambda message: user_states.get(message.chat.id) == DELPACK)
 def handle_document4(message):
     sticker_pack_link = message.text
-    sticker_pack_name = sticker_pack_link.split("/")[-1]
-    bot.delete_sticker_set(sticker_pack_name)
-    bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message( message.chat.id, jinxx_mess_start, parse_mode="Markdown")
-    user_states[message.chat.id] = HOME
+    if check_link(sticker_pack_link):
+        sticker_pack_name = sticker_pack_link.split("/")[-1]
+        try:
+            bot.delete_sticker_set(sticker_pack_name)
+            bot.send_chat_action(message.chat.id, 'typing')
+            bot.send_message( message.chat.id, jinxx_mess_start, parse_mode="Markdown")
+            user_states[message.chat.id] = HOME
+        except telebot.apihelper.ApiException as e:
+            if "STICKERSET_INVALID" in str(e):
+                bot.send_chat_action(message.chat.id, 'typing')
+                bot.send_message(message.chat.id, f"😢 Tʜɪs Mᴇᴛʜᴏᴅ Tᴏ Dᴇʟᴇᴛᴇ A Sᴛɪᴄᴋᴇʀ Pᴀᴄᴋ Fʀᴏᴍ A Sᴇᴛ Cʀᴇᴀᴛᴇᴅ Bʏ Tʜᴇ Bᴏᴛ.", parse_mode="Markdown")
+            else:
+                bot.send_chat_action(message.chat.id, 'typing')
+                bot.send_message(message.chat.id, f"```ERROR {e}```", parse_mode="Markdown")
+    else:
+        bot.send_chat_action(message.chat.id, 'typing')
+        bot.send_message(message.chat.id, f"❌ Tʜᴇ Mᴇssᴀɢᴇ Is Nᴏᴛ A Vᴀʟɪᴅ URL.\n\n```🔗Exᴀᴍᴘʟᴇ: https://t.me/addstickers/STICKER_NAME```", parse_mode="Markdown", reply_markup=send_st_pack_link_text)
+
+
+
+
+
 
 @bot.message_handler(commands=['addsticker'])
 def create_sticker_pack(message):
     user_states[message.chat.id] = ADD_LINK_STICKER
     bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message(message.chat.id, "🔗 Sᴇɴᴅ Sᴛɪᴄᴋᴇʀ Pᴀᴄᴋ Lɪɴᴋ")
+    bot.send_message(message.chat.id, "🔗 Sᴇɴᴅ Sᴛɪᴄᴋᴇʀ Pᴀᴄᴋ Lɪɴᴋ", reply_markup=send_st_pack_link_text)
 
-    
 @bot.message_handler(func=lambda message: user_states.get(message.chat.id) == ADD_LINK_STICKER)
 def handle_document3(message):
     user_id = str(message.from_user.id)
@@ -323,18 +350,21 @@ def handle_document2(message):
             webm_sticker=message.document.file_id
             )
             bot.send_chat_action(message.chat.id, 'typing')
-            bot.send_message(message.chat.id, f"{sticker_pack_cre_mess} https://t.me/addstickers/{sticker_pack_name}")
+            sticker_pack_link = f"https://t.me/addstickers/{sticker_pack_name}"
+            bot.send_message(message.chat.id, f"{sticker_pack_cre_mess} {sticker_pack_link}")
+            add_data_to_json(user_id, sticker_pack_link)
             user_states[message.chat.id] = HOME
-    
-        except Exception as e:
-            bot.send_chat_action(message.chat.id, 'typing')
-            bot.send_message(message.chat.id, e)
-    
-    
+        except telebot.apihelper.ApiException as e:
+            if "invalid sticker set name is specified" in str(e):
+                bot.send_chat_action(message.chat.id, 'typing')
+                bot.send_message(message.chat.id, "Network Error Send Again")
+            else:
+                bot.send_chat_action(message.chat.id, 'typing')
+                bot.send_message(message.chat.id, e)
     else:
         bot.send_chat_action(message.chat.id, 'typing')
         bot.send_message(message.chat.id, "📂 Sᴇɴᴅ Wᴇʙᴍ Sᴛɪᴄᴋᴇʀ Fɪʟᴇ")
     
     
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    bot.infinity_polling(none_stop=True)
